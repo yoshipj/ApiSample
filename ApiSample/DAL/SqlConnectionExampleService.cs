9dcc631e43f3d5f3f2b418582c55a48d8b47afc1
Marcin Niewiński <marcin.niewinski@itsol.tech>
2020-06-07 14:27:19 +0200

    Add MockDbService and start work with SqlConnectionService

---
+++ ApiSample/DAL/SqlConnectionExampleService.cs
@@ -0,0 +1,78 @@
+﻿using ApiSample.DTOs.Guests;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace ApiSample.DAL
+{
+    public class SqlConnectionExampleService : IExampleService
+    {
+        //Proszę tutaj wpisać swój connection string
+        private readonly string ConnStr = @"Data Source=db-mssql.pjwstk.edu.pl;Initial Catalog=yoshi;Integrated Security=True;";
+
+        public ICollection<GuestResponseDto> GetGuestsCollection(string lastName)
+        {
+            var listToReturn = new List<GuestResponseDto>();
+
+            using var sqlConn = new SqlConnection(ConnStr);
+            using var sqlCmd = new SqlCommand
+            {
+                Connection = sqlConn
+            };
+            if(string.IsNullOrEmpty(lastName))
+                sqlCmd.CommandText = @"SELECT g.IdGosc, g.Imie, g.Nazwisko, g.Procent_rabatu
+                                    FROM Gosc g;";
+            else
+            {
+                sqlCmd.CommandText = @"SELECT g.IdGosc, g.Imie, g.Nazwisko, g.Procent_rabatu
+                                       FROM Gosc g
+                                       WHERE g.Nazwisko = @LastName;";
+                sqlCmd.Parameters.AddWithValue("LastName", lastName);
+            }
+            sqlConn.Open();
+            using var reader = sqlCmd.ExecuteReader();
+            while(reader.Read())
+            {
+                var item = new GuestResponseDto
+                {
+                    IdGuest = int.Parse(reader["IdGosc"].ToString()),
+                    FirstName = reader["Imie"].ToString(),
+                    LastName = reader["Nazwisko"].ToString(),
+                    DiscountPercent = !string.IsNullOrEmpty(reader["Procent_rabatu"]?.ToString())
+                                        ? int.Parse(reader["Procent_rabatu"].ToString()) : (int?)null
+
+                };
+                listToReturn.Add(item);
+            }
+
+            return listToReturn;
+        }
+
+
+        public bool AddGuest(GuestRequestDto newGuest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteGuest(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public GuestResponseDto GetGuestById(int idGuest)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public string Test()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateGuest(int id, GuestRequestDto updateGuest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ApiSample/Startup.cs
+++ ApiSample/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ApiSample.DAL;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -18,12 +19,13 @@ namespace ApiSample
         {
             Configuration = configuration;
         }
-
+         
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IExampleService, SqlConnectionExampleService>();
             services.AddControllers();
         }
 
